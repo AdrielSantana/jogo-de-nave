140a89bfc43227c52ef097c9253eb090227c0cb0
Adriel Santana <diel_san@hotmail.com>
2025-01-29 15:47:32 -0300

    ci/cd

--- client/src/presentation/components/PlayerShip.tsx
+++ client/src/presentation/components/PlayerShip.tsx
@@ -1,26 +1,28 @@
-import { useRef } from "react";
-import { useFrame } from "@react-three/fiber";
+// import { useRef } from "react";
+// import { useFrame } from "@react-three/fiber";
 import { Spaceship } from "../../domain/entities/Spaceship";
-import { Mesh } from "three";
+// import { Mesh } from "three";
 
 interface PlayerShipProps {
   player: Spaceship;
 }
 
 export const PlayerShip = ({ player }: PlayerShipProps) => {
-  const meshRef = useRef<Mesh>(null);
+  console.log("PlayerShip", player);
+  // const meshRef = useRef<Mesh>(null);
 
-  useFrame((_, delta) => {
-    if (meshRef.current) {
-      meshRef.current.position.copy(player.position);
-      meshRef.current.quaternion.copy(player.rotation);
-    }
-  });
+  // useFrame((_, delta) => {
+  //   if (meshRef.current) {
+  //     meshRef.current.position.copy(player.position);
+  //     meshRef.current.quaternion.copy(player.rotation);
+  //   }
+  // });
 
   return (
-    <mesh ref={meshRef}>
-      <boxGeometry args={[1, 0.5, 2]} />
-      <meshStandardMaterial color="blue" />
-    </mesh>
+    <></>
+    // <mesh ref={meshRef}>
+    //   <boxGeometry args={[1, 0.5, 2]} />
+    //   <meshStandardMaterial color="blue" />
+    // </mesh>
   );
 };
